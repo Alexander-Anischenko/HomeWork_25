b47521d8e41b64eeab24a0bec3b1104bb1c0c5d4
Alexander-Anischenko <156853009+Alexander-Anischenko@users.noreply.github.com>
2024-05-07 12:38:29 +0200

    firstcommit

---
+++ src/homeWork_25/SumOfNumbers.java
@@ -0,0 +1,23 @@
+package homeWork_25;
+/* 1. Создайте проект
+2. Напишите программу которая считает сумму чисел от 1 до числа X
+(где значение X равняется произведению дня вашего рождения на год)*/
+//Поменяйте число X на число Y (где значение Y равняется произведению месяца вашего рождения на год)
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+
+        int x = 7 * 1979;
+        System.out.println("X = " + x);
+        int sum = calculateSum(x);
+        System.out.println("Sum of the numbers from 1 to " + x + " = " + sum);
+    }
+
+    private static int calculateSum(int x) {
+        int sum = 0;
+        for (int i = 1; i < x; i++) {
+            sum+= i;
+        }
+        return sum;
+    }
+}
